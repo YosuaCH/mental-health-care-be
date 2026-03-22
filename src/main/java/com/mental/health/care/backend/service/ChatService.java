c6a84be88189c94c68c1266bd6af51d58e6b1e6b
YosuaCH <yosuach38@gmail.com>
2026-03-22 17:54:06 +0700

    minor change logic on ChatService.java

--- src/main/java/com/mental/health/care/backend/service/ChatService.java
+++ src/main/java/com/mental/health/care/backend/service/ChatService.java
@@ -1,10 +1,15 @@
 package com.mental.health.care.backend.service;
 
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.google.genai.GoogleGenAiChatModel;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @Service
 public class ChatService {
@@ -21,8 +26,13 @@ public class ChatService {
     public String askGemini(String userMessage) {
         try {
             String systemInstruction = systemRulesResource.getContentAsString(StandardCharsets.UTF_8);
-            
-            return chatModel.call(systemInstruction + userMessage);
+
+            SystemMessage systemRole = new SystemMessage(systemInstruction);
+            UserMessage userRole = new UserMessage(userMessage);
+            Prompt prompt = new Prompt(List.of(systemRole, userRole));
+
+            return chatModel.call(prompt).getResult().getOutput().getText();
+
         } catch (Exception e) {
             e.printStackTrace();
             return "Maaf, asisten AI sedang tidak stabil: " + e.getMessage();
